e4ea5275c393913a8634341febac1f006aef7d14
Anton Antonov <antonov.ag@gmail.com>
2023-12-25 20:47:43 +0100

    Formatting Python code

--- load_config.py
+++ load_config.py
@@ -4,25 +4,33 @@ import argparse
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
-def load_config():
-    parser = argparse.ArgumentParser(description='Load Juniper Config')    
-    parser.add_argument('config', type=open, help='Name of the file containing the configuration to be uploaded')
-    parser.add_argument('format', type=str, help='Configuration file format')
-    parser.add_argument('device', type=str, help='IP address or domain name of the Juniper router')
-    parser.add_argument('-u','--user', type=str, help='User name')
-    parser.add_argument('-p','--passwd', type=str, help='Password')
-#    parser.add_argument('-k','--key', type=str, help='SSH private key file')
 
-    args = parser.parse_args()
-
-    with (Device(host=args.device, user=args.user, password=args.passwd).open()) as dev:
-        with Config(dev, mode='private') as cu:
-            cu.load(path=args.config.name, format=args.format)
+def load_config(device, user, password, config, format):
+    with Device(host=device, user=user, password=password).open() as dev:
+        with Config(dev, mode="private") as cu:
+            cu.load(path=config.name, format=format)
             cu.pdiff()
             cu.commit()
 
+
 # execute
 if __name__ == "__main__":
-    load_config()
+    parser = argparse.ArgumentParser(description="Load Juniper Config")
+    parser.add_argument(
+        "config",
+        type=open,
+        help="Name of the file containing the configuration to be uploaded",
+    )
+    parser.add_argument("format", type=str, help="Configuration file format")
+    parser.add_argument(
+        "device", type=str, help="IP address or domain name of the Juniper router"
+    )
+    parser.add_argument("-u", "--user", type=str, help="User name")
+    parser.add_argument("-p", "--password", type=str, help="Password")
+    #    parser.add_argument('-k','--key', type=str, help='SSH private key file')
+
+    args = parser.parse_args()
+
+    load_config(**vars(args))
 else:
     pass
--- read_config.py
+++ read_config.py
@@ -2,34 +2,51 @@
 
 import argparse
 from jnpr.junos import Device
-from jnpr.junos.utils.config import Config
 from lxml import etree
 
-def read_config():
-    parser = argparse.ArgumentParser(description='Read Juniper Config')    
-    parser.add_argument('-o','--output', type=argparse.FileType('w'), help='Write output to a file')
-    parser.add_argument('-m','--model', type=str, help='YANG data model: custom, ietf, openconfig')
-    parser.add_argument('-f','--filter', type=str, help='return a specific XML subtree')
-    parser.add_argument('format', type=str, help='Configuration output format: xml, text, set, json')
-    parser.add_argument('device', type=str, help='IP address or domain name of the Juniper router')
-    parser.add_argument('-u','--user', type=str, help='User name')
-    parser.add_argument('-p','--passwd', type=str, help='Password')
-#    parser.add_argument('-k','--key', type=str, help='SSH private key file')
 
-    args = parser.parse_args()
+def read_config(device, user, password, model, format, filter, output):
+    with Device(device, user, password) as dev:
+        data = dev.rpc.get_config(
+            filter_xml=filter, model=model, options={"format": format}
+        )
+    if format != "json":
+        data = etree.tostring(data, encoding="unicode", pretty_print=True)
+    if output is None:
+        print(data)
+        return
 
-    with (Device(host=args.device, user=args.user, password=args.passwd).open()) as dev:
-        data = dev.rpc.get_config(filter_xml=args.filter, model=args.model, options={'format':args.format})
-        if args.format != 'json':
-            data = etree.tostring(data, encoding='unicode', pretty_print=True)
-        if args.output:
-            with open(args.output.name, 'w') as file:
-                file.write(data)
-        else:
-            print (data)
+    with open(output.name, "w", encoding="utf-8") as file:
+        file.write(data)
+
 
 # execute
 if __name__ == "__main__":
-    read_config()
+    parser = argparse.ArgumentParser(description="Read Juniper Config")
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=None,
+        type=argparse.FileType("w"),
+        help="Write output to a file",
+    )
+    parser.add_argument(
+        "-m", "--model", type=str, help="YANG data model: custom, ietf, openconfig"
+    )
+    parser.add_argument(
+        "-f", "--filter", type=str, help="return a specific XML subtree"
+    )
+    parser.add_argument(
+        "format", type=str, help="Configuration output format: xml, text, set, json"
+    )
+    parser.add_argument(
+        "device", type=str, help="IP address or domain name of the Juniper router"
+    )
+    parser.add_argument("-u", "--user", type=str, help="User name")
+    parser.add_argument("-p", "--password", type=str, help="Password")
+
+    args = parser.parse_args()
+
+    read_config(**vars(args))
 else:
     pass
